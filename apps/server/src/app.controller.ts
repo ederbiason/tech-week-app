de96c869927f6d8b02a545644e00c3bfff0cb9cb
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-04-04 16:03:17 -0300

    feat: prisma setup

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common'
+import { PrismaService } from './database/prisma.service'
 
 @Controller()
 export class AppController {
+  constructor(private prisma: PrismaService) {}
+
   @Get()
-  getHello() {
-    return { message: 'Hello World' };
+  async getUsers() {
+    const users = await this.prisma.user.findMany()
+
+    return {
+      data: users,
+    }
   }
 }
