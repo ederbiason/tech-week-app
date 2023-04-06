7fe31398e4d84cc349c8da63c54c5342c94052a0
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-04-06 15:50:09 -0300

    feat: drawer setup

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,6 +2,19 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
+    plugins: [
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+      require.resolve('expo-router/babel'),
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@/*': './src/*',
+          },
+        },
+      ],
+    ],
   }
 }
